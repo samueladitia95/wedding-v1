b3be1aefbd6fd16f23391a5a7cb9fd2f7e44ca1a
Samuel Aditia <samueladitia95@gmail.com>
2023-01-25 23:30:17 +0700

    Test fetch data from api routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+async function getTestData(): Promise<{ name: string }> {
+	const res: Response = await fetch(`${process.env.BASE_URL}/api/hello`);
+	return res.json();
+}
+
+export default async function Home() {
+	const testData = await getTestData();
 	return (
 		<div className="container text-center">
 			<p>Hello World</p>
+			<p>{testData.name}</p>
 		</div>
 	);
 }
