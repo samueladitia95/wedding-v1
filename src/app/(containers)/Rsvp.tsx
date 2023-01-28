50a830ed70f18a4b1c9b8560a41b67ec90243e9a
Samuel Aditia <samueladitia95@gmail.com>
2023-01-28 21:58:27 +0700

    Add RSVP section container

---
+++ src/app/(containers)/Rsvp.tsx
@@ -0,0 +1,13 @@
+export default async function Rsvp() {
+	return (
+		<>
+			<div className="bg-secondary text-primary font-circular">
+				<div className="container py-20 flex flex-col gap-14 md:w-narrow">
+					<div className="uppercase">
+						<p className="font-serif text-center text-4xl">rsvp</p>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import MakeAWish from "./(containers)/MakeAWish";
+import Rsvp from "./(containers)/Rsvp";
 
 export default async function Home() {
 	return (
 		<div>
-			{/* Disable ts recommended by next js documentation */}
+			{/* Disable ts lint recommended by next js documentation */}
+
+			{/* @ts-expect-error Server Component */}
+			<Rsvp />
 			{/* @ts-expect-error Server Component */}
 			<MakeAWish />
 		</div>
