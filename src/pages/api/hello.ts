c88bdddef289a24bb09e7f9f7f1023a287deaf99
Samuel Aditia <samueladitia95@gmail.com>
2023-01-28 18:06:46 +0700

    Test rest api to handle http method get and post

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,12 +2,17 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-	name: string;
+	status: "success" | "failed";
+	name?: string;
 };
 
 export default function handler(
 	req: NextApiRequest,
 	res: NextApiResponse<Data>
 ) {
-	res.status(200).json({ name: "John Doe" });
+	if (req.method === "POST") {
+		res.status(201).json({ status: "failed" });
+	} else {
+		res.status(200).json({ status: "success", name: "John Doe" });
+	}
 }
