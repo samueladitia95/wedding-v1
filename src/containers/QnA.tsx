e07f2a4e42e0c4bb6910d5ba9331d5830d414dae
Samuel Aditia <samueladitia95@gmail.com>
2023-01-30 21:43:24 +0700

    Add responsiveness in QnA section

--- src/containers/QnA.tsx
+++ src/containers/QnA.tsx
@@ -36,19 +36,23 @@ const images1 = [
 	"https://via.placeholder.com/400x800",
 	"https://via.placeholder.com/400x500",
 	"https://via.placeholder.com/400x600",
+	"https://via.placeholder.com/400x800",
+	"https://via.placeholder.com/400x400",
 ];
 const images2 = [
 	"https://via.placeholder.com/400x400",
 	"https://via.placeholder.com/400x700",
 	"https://via.placeholder.com/400x800",
+	"https://via.placeholder.com/400x800",
+	"https://via.placeholder.com/400x400",
 ];
 
 const QnA = () => {
 	return (
 		<>
 			<div className="bg-cream text-secondary font-circular">
-				<div className="container py-20 flex flex-col items-center gap-14 md:w-narrow">
-					<div className="flex h-narrow overflow-auto w-full gap-4 rounded-2xl scrollbar-hide">
+				<div className="container py-20 flex flex-col items-center gap-14 md:w-narrow lg:flex-row lg:w-wide">
+					<div className="flex h-narrow overflow-auto w-full gap-4 rounded-2xl scrollbar-hide lg:h-wide lg:w-1/2 lg:overflow-y-auto">
 						<div className="flex flex-col gap-4 w-full ">
 							{images1.map((url, index) => (
 								<Image
@@ -80,64 +84,66 @@ const QnA = () => {
 						</div>
 					</div>
 
-					<div className="uppercase">
-						<p className="font-serif text-center text-4xl">
-							questions and answers
-						</p>
-					</div>
-
-					<div className="flex flex-col gap-10">
-						<div className="flex flex-col gap-6">
-							<div className="flex gap-3 uppercase  tracking-wider">
-								<p className="text-3xl font-bold">01</p>
-								<p className="text-sm font-medium">
-									What kind of outfit should shouldn&apos;t I wear?
-								</p>
-							</div>
-							<hr className="h-2 border-secondary" />
-							<div className="text-sm flex flex-col gap-8 font-medium leading-8 tracking-wide">
-								<p>
-									Be handsome and beautiful, being ugly is not tolerated (LOL).
-									Calling all Ladies! Long dresses are preferred and please wear
-									comfortable shoes. Get ready to be dancing all night with
-									booze involved.
-								</p>
-								<div className="flex flex-col gap-2">
-									<p>Here are the color tone for your dress</p>
-									<div className="flex gap-4">
-										{Array(4)
-											.fill(0)
-											.map((_, index) => (
-												<Image
-													src="https://via.placeholder.com/40"
-													alt="color"
-													width={40}
-													height={40}
-													key={index}
-													className="rounded-full"
-												/>
-											))}
-									</div>
-								</div>
-								<p>
-									For Gentlemen, suit up. <br /> Kindly leave your batik, jeans,
-									t-shirt, slippers at home.
-								</p>
-							</div>
+					<div className="flex flex-col gap-12 lg:w-1/2 lg:h-wide lg:overflow-y-auto scrollbar-hide">
+						<div className="uppercase">
+							<p className="font-serif text-center text-4xl">
+								questions and answers
+							</p>
 						</div>
 
-						{questions.map((el, index) => (
-							<div key={index} className="flex flex-col gap-6">
-								<div className="flex gap-4 items-center uppercase tracking-wider">
-									<p className="text-3xl font-bold">0{index + 2}</p>
-									<p className="text-sm font-medium">{el.question}</p>
+						<div className="flex flex-col gap-10 lg:h-wide">
+							<div className="flex flex-col gap-6">
+								<div className="flex gap-3 uppercase  tracking-wider">
+									<p className="text-3xl font-bold">01</p>
+									<p className="text-sm font-medium">
+										What kind of outfit should shouldn&apos;t I wear?
+									</p>
 								</div>
 								<hr className="h-2 border-secondary" />
-								<p className="font-medium leading-8 tracking-wide">
-									{el.answer}
-								</p>
+								<div className="text-sm flex flex-col gap-8 font-medium leading-8 tracking-wide">
+									<p>
+										Be handsome and beautiful, being ugly is not tolerated
+										(LOL). Calling all Ladies! Long dresses are preferred and
+										please wear comfortable shoes. Get ready to be dancing all
+										night with booze involved.
+									</p>
+									<div className="flex flex-col gap-2">
+										<p>Here are the color tone for your dress</p>
+										<div className="flex gap-4">
+											{Array(4)
+												.fill(0)
+												.map((_, index) => (
+													<Image
+														src="https://via.placeholder.com/40"
+														alt="color"
+														width={40}
+														height={40}
+														key={index}
+														className="rounded-full"
+													/>
+												))}
+										</div>
+									</div>
+									<p>
+										For Gentlemen, suit up. <br /> Kindly leave your batik,
+										jeans, t-shirt, slippers at home.
+									</p>
+								</div>
 							</div>
-						))}
+
+							{questions.map((el, index) => (
+								<div key={index} className="flex flex-col gap-6">
+									<div className="flex gap-4 items-center uppercase tracking-wider">
+										<p className="text-3xl font-bold">0{index + 2}</p>
+										<p className="text-sm font-medium">{el.question}</p>
+									</div>
+									<hr className="h-2 border-secondary" />
+									<p className="font-medium leading-8 tracking-wide">
+										{el.answer}
+									</p>
+								</div>
+							))}
+						</div>
 					</div>
 				</div>
 			</div>
