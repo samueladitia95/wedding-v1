92d256205fa22791f7430cfe10ddca50fc486de9
Samuel Aditia <samueladitia95@gmail.com>
2023-02-04 20:51:54 +0700

    update responsiveness for desktop in weekend guide

--- src/containers/WeekendGuide.tsx
+++ src/containers/WeekendGuide.tsx
@@ -1,60 +1,69 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const schedule1 = [
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "family arrival & check in",
-		time: "14.00 onwards",
-	},
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "family welcome dinner",
-		time: "18.00",
-	},
-];
+type Schedule = {
+	icon: string;
+	title: string;
+	location?: string;
+	time: string;
+	coordinate?: string;
+};
 
-const schedule2 = [
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "HOLY MATRIMONY",
-		location: "GEREJA KATOLIK SANTO SILVESTER",
-		time: "10.00",
-		coordinate: "www.google.com",
-	},
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "FAMILY TEA PAI CEREMONY",
-		location: "RADISSON BLU BALI, ULUWATU",
-		time: "13:00",
-		coordinate: "www.google.com",
-	},
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "SUNSET COCKTAIL",
-		location: "KAMAYA, BALI",
-		time: "17:30",
-	},
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "DINNER RECEPTION",
-		location: "KAMAYA, BALI",
-		time: "18:30",
-		coordinate: "www.google.com",
-	},
-	{
-		icon: "https://via.placeholder.com/64",
-		title: "LET’S DANCE!",
-		location: "KAMAYA, BALI",
-		time: "21:00",
-	},
+const schedules: Schedule[][] = [
+	[
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "family arrival & check in",
+			time: "14.00 onwards",
+		},
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "family welcome dinner",
+			time: "18.00",
+		},
+	],
+	[
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "HOLY MATRIMONY",
+			location: "GEREJA KATOLIK SANTO SILVESTER",
+			time: "10.00",
+			coordinate: "www.google.com",
+		},
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "FAMILY TEA PAI CEREMONY",
+			location: "RADISSON BLU BALI, ULUWATU",
+			time: "13:00",
+			coordinate: "www.google.com",
+		},
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "SUNSET COCKTAIL",
+			location: "KAMAYA, BALI",
+			time: "17:30",
+		},
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "DINNER RECEPTION",
+			location: "KAMAYA, BALI",
+			time: "18:30",
+			coordinate: "www.google.com",
+		},
+		{
+			icon: "https://via.placeholder.com/64",
+			title: "LET’S DANCE!",
+			location: "KAMAYA, BALI",
+			time: "21:00",
+		},
+	],
 ];
 
 const WeekendGuide = () => {
 	return (
 		<>
 			<div className="bg-olive font-circular flex flex-col items-center">
-				<div className="flex flex-col text-secondary text-center w-screen md:w-wide">
+				<div className="flex flex-col text-secondary text-center md:w-wide">
 					{/* TITLE */}
 					<div className="container bg-cream py-12 flex flex-col gap-4 items-center">
 						<div className="uppercase font-serif text-4xl font-medium">
@@ -73,45 +82,21 @@ const WeekendGuide = () => {
 					</div>
 
 					{/* Schedule */}
-					<div className="container flex flex-col gap-16 bg-primary text-secondary py-12">
-						{/* FRIDAY */}
-						<div className="flex flex-col gap-4 items-center">
-							<p className="font-baskervville italic text-brown">
-								Friday, June 2, 2023
-							</p>
-							<button className="uppercase p-4 border rounded border-brown w-full sm:w-80">
-								add to calender
-							</button>
-							<div className="flex flex-col gap-6">
-								{schedule1.map((el, index) => (
-									<div
-										key={index}
-										className="flex flex-col gap-4 items-center uppercase font-medium text-center"
-									>
-										<Image
-											src={el.icon}
-											alt="logo"
-											width={64}
-											height={64}
-											className="rounded"
-										/>
-										<p>{el.title}</p>
-										<p className="text-brown text-sm">{el.time}</p>
-									</div>
-								))}
-							</div>
-						</div>
-
-						{/* SATURDAY */}
-						<div className="flex flex-col gap-4 items-center">
-							<p className="font-baskervville italic text-brown">
-								Saturday, June 3, 2023
-							</p>
-							<button className="uppercase p-4 border rounded border-brown w-full sm:w-80">
-								add to calender
-							</button>
-							<div className="flex flex-col gap-6">
-								{schedule2.map((el, index) => (
+					<div className="container flex flex-col gap-14 bg-primary text-secondary py-12">
+						{schedules.map((day, index) => (
+							<div
+								className="flex flex-col gap-10 items-center md:flex-row md:items-start md:grid md:grid-cols-3 md:gap-4 md:gap-y-14"
+								key={index}
+							>
+								<div className="flex flex-col gap-4 w-full">
+									<p className="font-baskervville italic text-brown">
+										Saturday, June 3, 2023
+									</p>
+									<button className="uppercase p-4 border rounded border-brown w-full">
+										add to calender
+									</button>
+								</div>
+								{day.map((el, index) => (
 									<div
 										key={index}
 										className="flex flex-col gap-4 items-center uppercase text-center"
@@ -124,7 +109,9 @@ const WeekendGuide = () => {
 											className="rounded"
 										/>
 										<p className="font-semibold">{el.title}</p>
-										<p className="text-sm font-normal">{el.location}</p>
+										{el.location && (
+											<p className="text-sm font-normal">{el.location}</p>
+										)}
 										<p className="text-brown text-sm font-medium">{el.time}</p>
 										{el.coordinate && (
 											<Link
@@ -137,7 +124,7 @@ const WeekendGuide = () => {
 									</div>
 								))}
 							</div>
-						</div>
+						))}
 					</div>
 				</div>
 			</div>
