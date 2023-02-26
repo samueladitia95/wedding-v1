74e95534dc09b374803a173a23244e36afd22e73
Samuel Aditia <samueladitia95@gmail.com>
2023-02-26 16:10:39 +0700

    add animation to permanent text in intro component

--- src/containers/Intro.tsx
+++ src/containers/Intro.tsx
@@ -2,16 +2,32 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import And from "../../public/and.svg";
 
+// ? RIBET JUGA YA ANIMASI, GA HAFAL GW INI URUTANNYA BENER DEH
 const Intro = () => {
 	// ? trigger untuk dari tanda & besar ke kecil dan ganti background
 	const [firstAnimation, setFirstAnimation] = useState<boolean>(false);
 	// ? trigger untuk menampilkan nama groom and bride
 	const [secondAnimation, setSecondAnimation] = useState<boolean>(false);
+	// ? trigger untuk menghilangkan nama groom dan bride
+	const [thirdAnimation, setThirdAnimation] = useState<boolean>(false);
+
+	const [forthAnimation, setForthAnimation] = useState<boolean>(false);
+	const [fifthAnimation, setFifthAnimation] = useState<boolean>(false);
 
 	useEffect(() => {
 		setTimeout(() => {
 			setFirstAnimation(true);
-			setTimeout(() => setSecondAnimation(true), 1000);
+			setTimeout(() => {
+				setSecondAnimation(true);
+				setTimeout(() => {
+					setSecondAnimation(false);
+					setThirdAnimation(true);
+					setTimeout(() => {
+						setForthAnimation(true);
+						setTimeout(() => setFifthAnimation(true), 100);
+					}, 1000);
+				}, 1500);
+			}, 1000);
 		}, 500);
 	}, []);
 
@@ -26,69 +42,89 @@ const Intro = () => {
 					>
 						<div className="container pb-0 flex flex-col items-center md:w-ultra-wide h-tall">
 							<div className="text-center py-52 flex flex-col gap-10 md:w-narrow">
-								{/* <div
-									className={`uppercase font-safira text-4xl leading-9 transition-all duration-500 ease-out ${
-										!isIntro ? "opacity-0" : "opacity-100"
-									}`}
-								>
-									<p>I HOLD YOU IN MY HEART,</p>
-									<p>FOR WE HAVE SHARED TOGETHER GOD&apos;S BLESSINGS</p>
-								</div> */}
-
-								{/* <p className="italic font-baskervville text-lg">Philippians 1:7</p> */}
-								<div className="uppercase font-safira leading-9">
-									<p
-										className={`transition-all duration-1000 ease-in ${
-											!secondAnimation ? "opacity-0" : "opacity-100"
+								{forthAnimation && (
+									<div
+										className={`uppercase font-safira text-4xl leading-9 transition-all duration-1000 ease-in ${
+											!fifthAnimation ? "opacity-0" : "opacity-100"
 										}`}
 									>
-										the
-									</p>
-									<p
-										className={`font-baskervville transition-all duration-1000 ease-in ${
-											!secondAnimation ? "opacity-0" : "opacity-100"
-										}`}
-									>
-										wedding{" "}
-										<span className="italic lowercase text-3xl">of</span>
-									</p>
-									<p
-										className={`text-5xl transition-all duration-1000 ease-in ${
-											!secondAnimation ? "opacity-0" : "opacity-100"
-										}`}
-									>
-										irwan
-									</p>
-									<div className="flex justify-center items-center">
-										<Image
-											src={And}
-											alt="and symbol"
-											width={!firstAnimation ? 90 : 22}
-											height={!firstAnimation ? 160 : 40}
-											className="transition-all duration-1000 ease-linear"
-										/>
+										<p>I HOLD YOU IN MY HEART,</p>
+										<p>FOR WE HAVE SHARED TOGETHER GOD&apos;S BLESSINGS</p>
 									</div>
+								)}
+								{forthAnimation && (
 									<p
-										className={`text-5xl transition-all duration-1000 ease-in ${
-											!secondAnimation ? "opacity-0" : "opacity-100"
+										className={`italic font-baskervville text-lg transition-all duration-1000 ease-in ${
+											!fifthAnimation ? "opacity-0" : "opacity-100"
 										}`}
 									>
-										claudia
+										Philippians 1:7
 									</p>
-								</div>
+								)}
+								{forthAnimation && (
+									<div className="flex justify-center items-center mt-20">
+										<button>
+											<Image
+												src="https://via.placeholder.com/64"
+												alt="QR Code"
+												width={64}
+												height={64}
+												className={`rounded-full transition-all duration-1000 ease-in ${
+													!fifthAnimation ? "opacity-0" : "opacity-100"
+												}`}
+											/>
+										</button>
+									</div>
+								)}
+								{!forthAnimation && (
+									<div className="uppercase font-safira leading-9">
+										<p
+											className={`transition-all duration-1000 ease-in ${
+												!secondAnimation ? "opacity-0" : "opacity-100"
+											}`}
+										>
+											the
+										</p>
+										<p
+											className={`font-baskervville transition-all duration-1000 ease-in ${
+												!secondAnimation ? "opacity-0" : "opacity-100"
+											}`}
+										>
+											wedding{" "}
+											<span className="italic lowercase text-3xl">of</span>
+										</p>
+										<p
+											className={`text-5xl transition-all duration-1000 ease-in ${
+												!secondAnimation ? "opacity-0" : "opacity-100"
+											}`}
+										>
+											irwan
+										</p>
+										<div className="flex justify-center items-center">
+											<Image
+												src={And}
+												alt="and symbol"
+												width={!firstAnimation ? 90 : 22}
+												height={!firstAnimation ? 160 : 40}
+												className={`transition-all duration-1000 ease-linear ${
+													!secondAnimation && firstAnimation && thirdAnimation
+														? "opacity-0"
+														: "opacity-100"
+												}`}
+											/>
+										</div>
+										<p
+											className={`text-5xl transition-all duration-1000 ease-in ${
+												!secondAnimation ? "opacity-0" : "opacity-100"
+											}`}
+										>
+											claudia
+										</p>
+									</div>
+								)}
 							</div>
 						</div>
 					</div>
-
-					{/* <button>
-						<Image
-							src="https://via.placeholder.com/64"
-							alt="QR Code"
-							width={64}
-							height={64}
-							className="rounded-full"
-						/>
-					</button> */}
 				</div>
 			</div>
 		</>
