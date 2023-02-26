83191afb405a5642902d9aba6755f739388b1809
Samuel Aditia <samueladitia95@gmail.com>
2023-02-26 16:27:41 +0700

    fix overlay bug not strecting to edge

--- src/containers/BrideAndGroom.tsx
+++ src/containers/BrideAndGroom.tsx
@@ -6,30 +6,32 @@ const BrideAndGroom = () => {
 		<>
 			<div className="bg-soil8 text-soil2 font-circular">
 				<div className="bg-brideandgroom bg-cover bg-center bg-no-repeat">
-					<div className="container bg-black bg-opacity-50  pb-0 flex flex-col items-center gap-14 md:w-ultra-wide h-tall">
-						<div className="text-center pt-48 pb-0 flex flex-col gap-10">
-							<div className="uppercase font-baskervville text-4xl leading-9 tracking-widest font-medium flex flex-col gap-2">
-								<p className="text-left ml-6">irwan</p>
-								<p>thomas</p>
-								<p className="text-left">burhan</p>
-								<p>&</p>
-								<p className="text-right">Claudia</p>
-								<p className="text-right mr-6">Narmada</p>
+					<div className="bg-black bg-opacity-50">
+						<div className="container pb-0 flex flex-col items-center gap-14 md:w-ultra-wide h-tall">
+							<div className="text-center pt-48 pb-0 flex flex-col gap-10">
+								<div className="uppercase font-baskervville text-4xl leading-9 tracking-widest font-medium flex flex-col gap-2">
+									<p className="text-left ml-6">irwan</p>
+									<p>thomas</p>
+									<p className="text-left">burhan</p>
+									<p>&</p>
+									<p className="text-right">Claudia</p>
+									<p className="text-right mr-6">Narmada</p>
+								</div>
+								<p className="italic font-baskervville text-lg">
+									Let’s us tell you our classic love story
+								</p>
 							</div>
-							<p className="italic font-baskervville text-lg">
-								Let’s us tell you our classic love story
-							</p>
-						</div>
 
-						<button>
-							<Image
-								src={DownArrow}
-								alt="Down Arrow Button"
-								width={64}
-								height={64}
-								className="rounded-full"
-							/>
-						</button>
+							<button>
+								<Image
+									src={DownArrow}
+									alt="Down Arrow Button"
+									width={64}
+									height={64}
+									className="rounded-full"
+								/>
+							</button>
+						</div>
 					</div>
 				</div>
 			</div>
