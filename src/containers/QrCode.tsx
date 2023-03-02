8cb14b986537aeb33fa71f2b2f8c037b820e1fc4
Samuel Aditia <samueladitia95@gmail.com>
2023-03-03 01:06:12 +0700

    share love update

--- src/containers/QrCode.tsx
+++ src/containers/QrCode.tsx
@@ -6,11 +6,11 @@ const QrCode = () => {
 			<div className="bg-soil16 text-white font-circular">
 				<div className="container py-20 md:py-48 flex flex-col items-center gap-14 md:w-wide">
 					<div className="uppercase">
-						<p className="font-safira text-center text-5xl md:text-7xl">share love</p>
-						<p className="mt-4 text-center text-sm font-semibold">
+						<p className="font-safira text-center text-5xl md:text-7xl lg:text-8xl tracking-wide">share love</p>
+						<p className="mt-4 text-center tracking-wide lg:text-lg">
 							Your prayers is the best gift of all.
 						</p>
-						<p className="text-center text-sm font-semibold">
+						<p className="text-center tracking-wide lg:text-lg">
 							However if you want to give something extra, we will be happy to
 							receive it
 						</p>
