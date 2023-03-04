f179323205d0cc5ef40c0f19c88a2250329a9dae
Samuel Aditia <samueladitia95@gmail.com>
2023-03-04 15:19:46 +0700

    adjust text size in weekend guide

--- src/containers/WeekendGuide.tsx
+++ src/containers/WeekendGuide.tsx
@@ -73,12 +73,12 @@ const WeekendGuide = () => {
 							<p>weekend guide</p>
 						</div>
 
-						<div className="uppercase font-medium text-2xl">
-							<p>Hi there, here are the guide for you</p>
+						<div className="uppercase font-medium md:text-2xl">
+							<p className="whitespace-nowrap md:whitespace-normal">Hi there, here are the guide for you</p>
 							<p>to come to our wedding</p>
 						</div>
 
-						<div className="text-2xl border font-baskervville border-soil13 rounded-full p-4 italic">
+						<div className="text-lg md:text-2xl border font-baskervville border-soil13 rounded-full p-4 italic">
 							*All times are in WITA
 						</div>
 					</div>
