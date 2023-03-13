6bbc303b488ccb26ac9959dee63f71a6dc8fad50
Samuel Aditia <samueladitia95@gmail.com>
2023-03-13 18:43:10 +0700

    add gif to bride and groom

--- src/containers/BrideAndGroom.tsx
+++ src/containers/BrideAndGroom.tsx
@@ -10,7 +10,7 @@ const BrideAndGroom = () => {
 				<div className="bg-brideandgroom bg-cover bg-center bg-no-repeat">
 					<div className="bg-black bg-opacity-50">
 						<div className="container pb-0 flex flex-col items-center gap-14 md:w-ultra-wide h-tall">
-							<div className="text-center pt-48 pb-0 flex flex-col gap-10">
+							<div className="text-center pt-48 md:pt-0 2xl:-mt-20 pb-0 flex flex-col gap-10">
 								<div className="md:w-text-narrow uppercase font-safira text-4xl md:text-6xl leading-10 tracking-widest flex flex-col gap-2 md:hidden">
 									<p className="text-left ml-6">irwan</p>
 									<p>thomas</p>
@@ -22,12 +22,12 @@ const BrideAndGroom = () => {
 								<Image
 									src={BrideAndGroomGif}
 									alt="Bride and groom"
-									width={9000}
-									height={9000}
+									width={900}
+									height={900}
 									priority
 									className="z-10 w-screen hidden md:block"
 								/>
-								<p className="italic font-baskervville text-2xl md:text-3xl">
+								<p className="italic font-baskervville text-2xl md:text-3xl lg:-mt-24 xl:-mt-36">
 									Let’s us tell you our classic journey
 								</p>
 							</div>
