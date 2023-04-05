3b8d3a4ffc749d52b55becbc13c86afb561a0d9c
rezafiansyah <rezafiansyah.putra@gmail.com>
2023-04-05 21:10:01 +0700

    Adding logic for fetch and post data

--- src/pages/api/comments.ts
+++ src/pages/api/comments.ts
@@ -2,84 +2,57 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "@/lib/initSupabase";
 import { isoToLongDate } from "@/utils/dateFormatter";
-
-type Data = {
-	status: number;
-	statusText: string;
-	comments?: {
-		name: string;
-		comment: string;
-		createdAt: string;
-	}[];
-};
-
-const dummyData: Data = {
-	status: 200,
-	statusText: "OK",
-	comments: [
-		{
-			name: "John Doe",
-			comment:
-				"Congrats on your wedding! I'm so happy for both of you! See you soon in Bali!",
-			createdAt: "Saturday, 13 May 2023  10:00",
-		},
-		{
-			name: "John Doe",
-			comment:
-				"Congrats on your wedding! I'm so happy for both of you! See you soon in Bali!",
-			createdAt: "Saturday, 13 May 2023  10:00",
-		},
-		{
-			name: "John Doe",
-			comment:
-				"Congrats on your wedding! I'm so happy for both of you! See you soon in Bali!",
-			createdAt: "Saturday, 13 May 2023  10:00",
-		},
-		{
-			name: "John Doe",
-			comment:
-				"Congrats on your wedding! I'm so happy for both of you! See you soon in Bali!",
-			createdAt: "Saturday, 13 May 2023  10:00",
-		},
-		{
-			name: "John Doe",
-			comment:
-				"Congrats on your wedding! I'm so happy for both of you! See you soon in Bali!",
-			createdAt: "Saturday, 13 May 2023  10:00",
-		},
-	],
-};
+import { WishlistData } from "@/type";
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<Data>
+	res: NextApiResponse<WishlistData>
 ) {
-	if (req.method === "POST") {
-		res.status(201).json({ status: 201, statusText: "Created" });
-	} else {
-		// get the page for pagination
-		const {
-			data: wishes,
-			error,
-			status,
-			statusText,
-		} = await supabase.from("wishes").select("*").range(0, 9);
+	const { body, method } = req;
+	if (method === "POST") {
+		try {
+			const { name, comment } = body;
+			const { status, statusText } = await supabase
+				.from("wishes")
+				.insert([{ name, wish: comment }]);
 
-		if (error) {
+			return res.status(status).json({ status, statusText });
+		} catch (error) {
+			return res
+				.status(500)
+				.json({ status: 500, statusText: "failed to insert data" });
+		}
+	} else {
+		try {
+			const { query } = req;
+			const { limit } = query;
+
+			const wishLimit: number = Number(limit) - 1 || 9;
+			const {
+				data: wishes,
+				status,
+				statusText,
+			} = await supabase
+				.from("wishes")
+				.select("*")
+				.range(0, wishLimit)
+				.order("created_at", { ascending: false });
+
+			return res.status(200).json({
+				status,
+				statusText,
+				comments: wishes?.map((el) => ({
+					name: el.name,
+					comment: el.wish,
+					createdAt: isoToLongDate(el.created_at),
+				})),
+			});
+		} catch (error) {
 			console.log(error);
+
 			return {
-				props: { comments: dummyData },
+				props: { comments: [] },
 			};
 		}
-
-		res.status(200).json({
-			status,
-			statusText,
-			comments: wishes.map((el) => ({
-				name: el.name,
-				comment: el.wish,
-				createdAt: isoToLongDate(el.created_at),
-			})),
-		});
 	}
 }
