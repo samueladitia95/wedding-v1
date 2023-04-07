7f4ff41b21f30138ad3413205a9f896abe576fd5
rezafiansyah <rezafiansyah.putra@gmail.com>
2023-04-07 22:26:38 +0700

    change to original to avoid stretched

--- src/containers/Intro.tsx
+++ src/containers/Intro.tsx
@@ -45,7 +45,7 @@ const Intro = ({
 				>
 					<div className="z-10 h-screen w-full absolute">
 						<Image
-							src="https://digital-invitation-1.s3.ap-southeast-1.amazonaws.com/irwanclaudia/Desktop/landing_page_background_desktop_resize.jpg"
+							src="https://digital-invitation-1.s3.ap-southeast-1.amazonaws.com/irwanclaudia/Desktop/landing_page_background_desktop_original.jpg"
 							alt="Background"
 							fill
 							className="object-cover"
